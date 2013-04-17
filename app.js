af94d3233ad610cd5760e4abf530d2f758fffd85
Jun Komatsu <jun@uniba.jp>
2013-04-17 21:16:48 +0900

    - Facebook / Twitter での認証 - アイコン、名前の取得

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
+  , config = require('config')
+  , passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy
+  , TwitterStrategy = require('passport-twitter').Strategy
   , Motors = require('./motors')
   , Display = require('./display');
 
@@ -21,8 +25,17 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+  app.use(express.cookieParser('krobotsecret'));
+  app.use(express.session());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+var FACEBOOK_APP_ID = config.FACEBOOK_APP_ID;
+var FACEBOOK_APP_SECRET = config.FACEBOOK_APP_SECRET;
+var TWITTER_CONSUMER_KEY = config.TWITTER_CONSUMER_KEY;
+var TWITTER_CONSUMER_SECRET = config.TWITTER_CONSUMER_SECRET;
 
 // development only
 if ('development' == app.get('env')) {
@@ -32,10 +45,92 @@ if ('development' == app.get('env')) {
 var motors = new Motors();
 var display = new Display();
 
-app.get('/', function(req,res) {
-  app.locals.host = req.host;
-  res.render('index');
+// initialize passport
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
+
+passport.deserializeUser(function(id, done) {
+  done(null, id);
+});
+
+passport.use(new FacebookStrategy(
+{
+  clientID: FACEBOOK_APP_ID,
+  clientSecret: FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3000/auth/facebook/callback",
+},
+function(accessToken, refreshToken, profile, done) {
+  passport.session.accessToken = accessToken;
+  passport.session.profile = profile;
+  console.log(profile);
+  process.nextTick(function() {
+    profile.image = 'https://graph.facebook.com/' + profile.username + '/picture';
+    done(null, profile);
+  });
+}));
+
+
+passport.use(new TwitterStrategy(
+{
+  consumerKey: TWITTER_CONSUMER_KEY,
+  consumerSecret: TWITTER_CONSUMER_SECRET,
+  callbackURL: "http://localhost:3000/auth/twitter/callback"
+},
+function(token, tokenSecret, profile, done) {
+  passport.session.accessToken = token;
+  passport.session.profile = profile;
+  console.log(profile);
+  process.nextTick(function () {
+    profile.image = profile.photos[0].value;
+    return done(null, profile);
+  });
+}));
+
+app.get('/', function(req,res) {
+
+  console.log(req.user);
+  if (req.user == undefined) {
+    res.redirect('/login');
+  } else {
+    app.locals.host = req.host;
+    app.locals.provider = req.user.provider;
+    app.locals.image = req.user.image;
+    if (req.user.provider == 'twitter') {
+      app.locals.username = '@' + req.user.username;
+    } else {
+      app.locals.username = req.user.displayName;
+    }
+    res.render('index');
+  }
+});
+
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', {failureRedirect: '/auth/fail'}),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+app.get('/auth/twitter/callback', 
+  passport.authenticate('twitter', { failureRedirect: '/auth/fail'}),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/login');
+});
+
 app.get('/go', function(req, res) {
   motors.go();
   res.send('ok');
--- display.js
+++ display.js
@@ -1,4 +1,6 @@
-var exec = require('child_process').exec;
+var exec = require('child_process').exec
+  , request = require('request')
+  , fs = require('fs');
 
 /**
  * Control Display (Linux frame buffer) by fim (framebuffer image viewer improve) command
@@ -15,8 +17,23 @@ Display.prototype = {
 
   display : function(filename) {
     exec('fim -a ' + filename);
-  }
+  }, 
 
+  display_url : function(url) {
+    request(url, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var filename = new Date().getTime();
+        fs.writeFile("/tmp/" + filename, body, function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("URL : " + url + " write to /tmp/" + filename);
+            display("/tmp/" + filename);
+          }
+        });
+      }
+    })
+  }
 };
 
 module.exports = Display;
